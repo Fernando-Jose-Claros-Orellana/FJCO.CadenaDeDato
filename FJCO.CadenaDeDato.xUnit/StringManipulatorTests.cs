8131d61266cb03e119487ecd82ac7baa05a8dc09
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-10-09 15:24:33 -0600

    Se agrega clase para ejecutar prueba

---
+++ FJCO.CadenaDeDato.xUnit/StringManipulatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FJCO.CadenaDeDato.xUnit
+{
+    public class StringManipulatorTests
+    {
+        [Fact]
+        public void ReverseString()
+        {
+            // Arrange
+            var stringManipulator = new StringManipulator();
+            var input = "Hello World!";
+            var expected = "!dlroW olleH";
+
+            // Act
+            var result = stringManipulator.ReverseString(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void RemoveSpaces()
+        {
+            // Arrange
+            var stringManipulator = new StringManipulator();
+            var input = "Hello World!";
+            var expected = "HelloWorld!";
+
+            // Act
+            var result = stringManipulator.RemoveSpaces(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
